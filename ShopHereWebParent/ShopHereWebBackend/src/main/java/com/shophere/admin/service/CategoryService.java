a066922a618c297c9803987697c68a4063b55dda
HOME <klintonece@gmail.com>
2023-08-25 01:10:54 +0530

    Commit Loop 70 Completed

--- ShopHereWebParent/ShopHereWebBackend/src/main/java/com/shophere/admin/service/CategoryService.java
+++ ShopHereWebParent/ShopHereWebBackend/src/main/java/com/shophere/admin/service/CategoryService.java
@@ -1,6 +1,8 @@
 package com.shophere.admin.service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,10 +18,33 @@ public class CategoryService {
 	public List<Category> listAll() {
 		return catagoryRepository.findAll();
 	}
-	
-	public List<Category>listCategoriesUsedInForm()
-	{
-		return catagoryRepository.findAll();
+
+	public List<Category> listCategoriesUsedInForm() {
+		List<Category> categoriesUsedInForm = new ArrayList<>();
+		Iterable<Category> categoriesInDB = catagoryRepository.findAll();
+		for (Category category : categoriesInDB) {
+			if (category.getParent() == null) {
+				categoriesUsedInForm.add(new Category(category.getName()));
+				Set<Category> children = category.getChild();
+				for (Category subCategory : children) {
+					categoriesUsedInForm.add(new Category("--" + subCategory.getName()));
+					printChildren(categoriesUsedInForm, subCategory, 1);
+				}
+			}
+		}
+		return categoriesUsedInForm;
 	}
 
+	private void printChildren(List<Category> categoriesUsedInForm, Category parent, int sublevel) {
+		int newLevel = sublevel + 1;
+		Set<Category> children = parent.getChild();
+		for (Category subCategory : children) {
+			String name = "";
+			for (int i = 0; i < newLevel; i++)
+				name += "--";
+			name += subCategory.getName();
+			categoriesUsedInForm.add(new Category(name));
+			printChildren(categoriesUsedInForm, subCategory, newLevel);
+		}
+	}
 }
